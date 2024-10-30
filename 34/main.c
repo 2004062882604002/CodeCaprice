01d3156119aa4784484b7d2cd0bbd4f1e674f7cb
2004062882604002 <133771432+2004062882604002@users.noreply.github.com>
2024-10-30 20:59:54 +0800

    Leetcode34 1.0

---
+++ 34/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize);
+
+int main(void)
+{
+    printf("Hello, from 34!\n");
+}
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int left = 0;
+    int right = numsSize - 1;
+    int sym = 0;
+    returnSize[2] = {-1, -1};
+    while(left <= right)
+    {
+        int middle = (right + left) / 2;
+        if(target < nums[middle])
+        {
+            right = middle - 1;
+        }
+        else if(target > nums[middle])
+        {
+            left = middle + 1;
+        }
+        else
+        {
+            if(sym == 0)
+                returnSize[sym++] = middle;
+            else
+                returnSize[sym] = middle;
+        }
+    }
+    return returnSize;
+}
