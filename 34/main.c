56ac319ff167c823ee082c28d223947fc9073ff3
2004062882604002 <133771432+2004062882604002@users.noreply.github.com>
2024-10-30 21:23:02 +0800

    Leetcod34 问题挺大的感觉二分，直接给我把“好数字“给我跳过了

--- 34/main.c
+++ 34/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int* searchRange(int* nums, int numsSize, int target, int* returnSize);
 
 int main(void)
 {
+    int nums[6] = {5,7,7,8,8,10};
+    int *returnSize;
+
+    searchRange(nums, 6, 8, returnSize);
+    
+    for(int i = 0; i < 2; i++)
+    {
+        printf("[%d, %d]", returnSize[0], returnSize[1]);
+    }
     printf("Hello, from 34!\n");
 }
 
@@ -15,6 +25,7 @@ int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
     int left = 0;
     int right = numsSize - 1;
     int sym = 0;
+    returnSize = (int *)malloc(2 * sizeof(int));
     returnSize[0] = -1;
     returnSize[1] = -1;
     while(left <= right)
