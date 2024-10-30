6cdc43aa468529685917a2e19d18edc219ace62a
2004062882604002 <133771432+2004062882604002@users.noreply.github.com>
2024-10-30 21:59:58 +0800

    Leetcode27 1.0暴力解法

---
+++ 27/build/CMakeFiles/27.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "27.exe"
+  "27.exe.manifest"
+  "27.pdb"
+  "CMakeFiles/27.dir/main.c.obj"
+  "CMakeFiles/27.dir/main.c.obj.d"
+  "lib27.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/27.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 27/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val);
+int main(void)
+{
+    int nums[8] = {0,1,2,2,3,0,4,2};
+    int res = 0;
+    res = removeElement(nums, 8, 2);
+    printf("%d", res);
+    return 0;
+}
+
+int removeElement(int* nums, int numsSize, int val) {
+    int sym = 0;
+    int length = numsSize;
+    for(int i = 0; i < length; i++)
+    {
+        if(nums[i] == val)
+        {
+            sym ++;
+            for(int j = i; j < length - 1; j++)
+            {
+                nums[j] = nums[j + 1];
+            }
+            length--;
+            i--;
+        }
+    }
+    return numsSize - sym;
+}
