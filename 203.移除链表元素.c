3c4bbd45c927e64051dc224d7ff87fcb38761a1e
2004062882604002 <133771432+2004062882604002@users.noreply.github.com>
2024-11-03 05:24:43 +0800

    Leetcode203 1.0虚拟头节点

---
+++ 203.移除链表元素.c
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=c
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    typedef struct ListNode ListNode;
+    ListNode* dNode;
+    dNode = (ListNode* )malloc(sizeof(ListNode));
+    dNode->next = head;
+    ListNode* cru = dNode;
+    while(cru->next != NULL)
+    {
+        if(cru->next->val == val)
+        {
+            ListNode* tmp = cru->next;
+            cru->next = cru->next->next;
+            free(tmp);
+        }
+        else
+        {
+            cru = cru->next;
+        }
+    }
+    head = dNode->next;
+    free(dNode);
+    return head;
+}
+// @lc code=end
+
---
+++ 203/build/CMakeFiles/203.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "203.exe"
+  "203.exe.manifest"
+  "203.pdb"
+  "CMakeFiles/203.dir/main.c.obj"
+  "CMakeFiles/203.dir/main.c.obj.d"
+  "lib203.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/203.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
