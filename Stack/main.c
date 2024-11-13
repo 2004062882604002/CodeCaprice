b3249cbee6d82bc6666b4b44030a6cd73b60292e
2004062882604002 <133771432+2004062882604002@users.noreply.github.com>
2024-11-13 16:48:36 +0800

    C语言链表实现2.0——修改pop

--- Stack/main.c
+++ Stack/main.c
@@ -52,3 +52,40 @@ int peek(StackNode* top)
     int top_value = top->value;
     return top_value;
 }
+
+// 2.0修改了pop，只要了当前值
+typedef struct StackNode {
+    int value;
+    struct StackNode* next;
+} StackNode;
+
+StackNode* createStack() {
+    return NULL;
+}
+
+void push(StackNode** top, int value) {
+    StackNode* NewTop = (StackNode*)malloc(sizeof(StackNode));
+    NewTop->next = *top;
+    NewTop->value = value;
+    *top = NewTop;
+}
+
+int pop(StackNode** top) {
+    if (*top == NULL) {
+        return -1;
+    }
+    StackNode* temp = *top;
+    int top_value = temp->value;
+    *top = temp->next;
+    free(temp);
+    return top_value;
+}
+
+int peek(StackNode* top) {
+    if (top == NULL) {
+        // Handle empty stack if needed
+        return -1;
+    }
+    int top_value = top->value;
+    return top_value;
+}
